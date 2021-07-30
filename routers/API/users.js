6f0b8ab1574f0ab1993f2cc06934ba09ff8a6f50
Киселев Михаил В <mvkiselev@group.legion.ru>
2021-07-30 13:13:35 +0300

    Version 20210730 13:13

--- routers/API/users.js
+++ routers/API/users.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const store = require('../../store/users')
+const TarantoolConnection = require('tarantool-driver');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     console.log("Find Users ...")
     console.log(req.query.FirstName)
     console.log(req.query.LastName)
-    store.getListByNames(req.query.FirstName, req.query.LastName)
-    .then(data => res.sendStatus(200))
-    .catch(error => {
-        console.log(error)
-        res.sendStatus(500)
-    });   
+    if (req.query.tarantool) {
+        console.log('Tarantool Mode ...')
+        const conn = new TarantoolConnection('bxu.group.legion.ru:3301'); //'10.106.101.117'
+
+        const users = await conn.call('getUsers', req.query.FirstName, req.query.LastName)
+        console.log(users)
+        return res.status(200).send(users)    
+    } else {
+        store.getListByNames(req.query.FirstName, req.query.LastName)
+        .then(data => res.sendStatus(200))
+        .catch(error => {
+            console.log(error)
+            res.sendStatus(500)
+        });   
+    }
 })
 
 module.exports = router
--- tarantool.js
+++ tarantool.js
@@ -24,24 +24,51 @@ main()
     const currentUser = await conn.eval('return box.session.user()')
     console.log(currentUser);
 
-    await conn.eval("box.schema.space.create('msnusers')")
-    await conn.eval("box.space.msnusers:create_index('msnusers_primary_idx')")
+    //await conn.eval("box.space.mysocionet:drop()")
+    //console.log('Space Dropped ...')
+
+    await conn.eval("box.schema.space.create('mysocionet')")
+    console.log('Space Created ...')
+
+    await conn.eval("box.space.mysocionet:create_index('mysocionet_primary_idx')")
     
     const chunkSize = 10000;
     const blocksNum = Math.ceil(totalRows / chunkSize);
 
     for(let i = 0; i < blocksNum; i++) {
         const offset = i * chunkSize;
-        const [rows, fields] = await (await connection).query('SELECT id, email, CryptoPassword FROM Users LIMIT ' + offset + ',' + chunkSize, []);
+        const [rows, fields] = await (await connection).query('SELECT id, email, CryptoPassword, FirstName, LastName FROM Users LIMIT ' + offset + ',' + chunkSize, []);
         for(let j = 0; j < rows.length; j++) {
-            await conn.insert("msnusers", [rows[j].id, rows[j].email, rows[j].CryptoPassword])
+            await conn.insert("mysocionet", [rows[j].id, rows[j].email, rows[j].CryptoPassword, rows[j].FirstName, rows[j].LastName])
         }
         console.log(`Block: ${i}`);
     }
+    
+    await conn.eval("box.space.mysocionet:create_index('mysocionet_secondary_idx', {unique = false, parts = {{field = 4, type = 'string'}, {field = 5, type = 'string'}}})")
+    
     let response = await conn.eval('return box.space');
     console.log(response);
-    response = await conn.eval('return box.space.msnusers:len()');
+    response = await conn.eval('return box.space.mysocionet:len()');
     console.log(response);
+
+    const users = await conn.call('getUsers', 'Donaugh', 'Borres')
+    console.log(users)
 
 })
 .catch(error => console.log(error))
+
+// main()
+// .then(async () => {
+//     const conn = new TarantoolConnection('bxu.group.legion.ru:3301'); //'10.106.101.117'
+    
+//     const currentUser = await conn.eval('return box.session.user()')
+//     console.log(currentUser);
+
+//     const spaces = await conn.eval('return box.space');
+//     console.log(spaces);
+
+//     const users = await conn.call('getUsers', 'Donaugh', 'Borres')
+//     console.log(users)
+
+// })
+// .catch(error => console.log(error))
